65ded7390597a3c95b59de3a7001eac481eb9628
Alexander Johansson <alexander@n1s.se>
2025-06-01 18:33:29 +0200

    some stuff

---
+++ src/createDeferred.ts
@@ -0,0 +1,13 @@
+export type Deferred<TValue> = ReturnType<typeof createDeferred<TValue>>;
+
+export function createDeferred<TValue = void>() {
+	let resolve: (value: TValue) => void;
+	let reject: (error: unknown) => void;
+	const promise = new Promise<TValue>((res, rej) => {
+		resolve = res;
+		reject = rej;
+	});
+
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	return { promise, reject: reject!, resolve: resolve! };
+}
---
+++ src/makeAsyncResource.ts
@@ -0,0 +1,26 @@
+// @ts-expect-error - polyfilling symbol
+
+Symbol.asyncDispose ??= Symbol();
+
+/**
+ * Takes a value and an async dispose function and returns a new object that implements the AsyncDisposable interface.
+ * The returned object is the original value augmented with a Symbol.asyncDispose method.
+ * @param thing The value to make async disposable
+ * @param dispose Async function to call when disposing the resource
+ * @returns The original value with Symbol.asyncDispose method added
+ */
+export function makeAsyncResource<T>(
+	thing: T,
+	dispose: () => Promise<void>,
+): AsyncDisposable & T {
+	const it = thing as Partial<AsyncDisposable> & T;
+
+	const existing = it[Symbol.asyncDispose];
+
+	it[Symbol.asyncDispose] = async () => {
+		await dispose();
+		await existing?.();
+	};
+
+	return it as AsyncDisposable & T;
+}
