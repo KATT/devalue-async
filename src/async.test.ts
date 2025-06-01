1a49a49bcd20b85cc6acffdf7a8ee86ead4b6468
Alexander Johansson <alexander@n1s.se>
2025-06-01 21:08:41 +0200

    wip

--- src/async.test.ts
+++ src/async.test.ts
@@ -26,6 +26,9 @@ async function* asyncIterableFrom<T>(
 	}
 }
 
+const run = <TResult>(fn: () => TResult): TResult => fn();
+
+type Constructor<T extends object = object> = new (...args: any[]) => T;
 function readableStreamFrom<T>(iterable: AsyncIterable<T>) {
 	const iterator = iterable[Symbol.asyncIterator]();
 
@@ -45,6 +48,34 @@ function readableStreamFrom<T>(iterable: AsyncIterable<T>) {
 			controller.enqueue(result.value);
 		},
 	});
+}
+
+async function waitError<TError extends Error = Error>(
+	/**
+	 * Function callback or promise that you expect will throw
+	 */
+	fnOrPromise: (() => unknown) | Promise<unknown>,
+
+	/**
+	 * Force error constructor to be of specific type
+	 * @default Error
+	 */
+	errorConstructor?: Constructor<TError>,
+): Promise<TError> {
+	try {
+		if (typeof fnOrPromise === "function") {
+			await fnOrPromise();
+		} else {
+			await fnOrPromise;
+		}
+	} catch (cause) {
+		expect(cause).toBeInstanceOf(Error);
+		if (errorConstructor) {
+			expect((cause as Error).name).toBe(errorConstructor.name);
+		}
+		return cause as TError;
+	}
+	throw new Error("Function did not throw");
 }
 
 test("stringify and unflatten async", async () => {
@@ -88,7 +119,81 @@ test("stringify and unflatten async", async () => {
 	expect(aggregate).toEqual([-0, 1, 2]);
 });
 
-test("stringify and parse async values with errors", async () => {
+test("stringify and parse async values with errors - simple", async () => {
+	class MyCustomError extends Error {
+		constructor(message: string) {
+			super(message);
+			this.name = "MyCustomError";
+		}
+	}
+
+	class UnregisteredError extends Error {
+		constructor(cause: unknown) {
+			const message = cause instanceof Error ? cause.message : String(cause);
+			super(message, { cause });
+			this.name = "UnregisteredError";
+		}
+	}
+
+	const source = {
+		asyncIterable: (async function* () {
+			yield -0;
+			throw new MyCustomError("error in async iterable");
+		})(),
+	};
+
+	const iterable = stringifyAsync(source, {
+		coerceError: (error) => {
+			return new UnregisteredError(error);
+		},
+		revivers: {
+			MyCustomError: (value) => {
+				if (value instanceof MyCustomError) {
+					return value.message;
+				}
+				return false;
+			},
+			UnregisteredError: (value) => {
+				if (value instanceof UnregisteredError) {
+					return [value.message];
+				}
+				return false;
+			},
+		},
+	});
+
+	async function* withDebug<T>(iterable: AsyncIterable<T>) {
+		for await (const value of iterable) {
+			yield value;
+			// console.log('yielding', value)
+		}
+	}
+
+	const result = await unflattenAsync<typeof source>(withDebug(iterable), {
+		reducers: {
+			MyCustomError: (value) => {
+				return new MyCustomError(value as string);
+			},
+			UnregisteredError: (...args) => {
+				return new UnregisteredError(...args);
+			},
+		},
+	});
+
+	const aggregate: number[] = [];
+	const err = await waitError(
+		(async () => {
+			for await (const value of result.asyncIterable) {
+				aggregate.push(value);
+			}
+		})(),
+	);
+	expect(err).toBeInstanceOf(MyCustomError);
+	expect(err.message).toEqual("error in async iterable");
+	expect(aggregate).toEqual([-0]);
+});
+
+test.only("stringify and parse async values with errors", async () => {
 	class MyCustomError extends Error {
 		constructor(message: string) {
 			super(message);
@@ -158,41 +263,28 @@ test("stringify and parse async values with errors", async () => {
 		},
 	});
 
-	try {
-		await result.unknownErrorDoesNotBlockStream;
-		throw new Error("expected error");
-	} catch (e) {
-		if (!(e instanceof Error)) {
-			throw new Error("expected error");
-		}
-		expect(e).toBeInstanceOf(UnregisteredError);
-		expect(e.message).toEqual("unknown error");
+	{
+		const err = await waitError(
+			result.unknownErrorDoesNotBlockStream,
+			UnregisteredError,
+		);
+		expect(err).toMatchInlineSnapshot(`[UnregisteredError: unknown error]`);
 	}
-	try {
-		await result.promise;
-		throw new Error("expected error");
-	} catch (e: unknown) {
-		if (!(e instanceof Error)) {
-			throw new Error("expected error");
-		}
-		expect(e).toBeInstanceOf(MyCustomError);
-		expect(e.message).toEqual("error in promise");
+	{
+		const err = await waitError(result.promise, MyCustomError);
+		expect(err).toMatchInlineSnapshot(`[MyCustomError: error in promise]`);
 	}
+	{
+		const aggregate: number[] = [];
 
-	const aggregate = [];
-	try {
-		for await (const value of result.asyncIterable) {
-			aggregate.push(value);
-		}
-	} catch (e) {
-		if (!(e instanceof Error)) {
-			throw new Error("expected error");
-		}
-		expect(e).toBeInstanceOf(MyCustomError);
-		expect(e.message).toEqual("error in async iterable");
-	}
+		await waitError(async () => {
+			for await (const value of result.asyncIterable) {
+				aggregate.push(value);
+			}
+		}, MyCustomError);
 
-	expect(aggregate).toEqual([-0]);
+		expect(aggregate).toEqual([-0]);
+	}
 });
 
 test("request/response-like readable streams", async () => {
